d5b17daf568c726382d6535b3e3a63555b10791d
Divyanshi Agrawal <divsagr3@gmail.com>
2020-08-29 20:05:53 +0530

    mat-ui

--- src/App.js
+++ src/App.js
@@ -3,8 +3,32 @@ import "./App.css";
 import Post from "./Post";
 import { db } from "./firebase";
 
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 function App() {
+  const classes = useStyles();
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
 
   //useEffect -> Runs a piece of code based on a specific condition
 
@@ -23,6 +47,12 @@ function App() {
 
   return (
     <div className="App">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <h2>I am a modal.</h2>
+        </div>
+      </Modal>
+
       <div className="app-header">
         <img
           className="app-headerImage"
