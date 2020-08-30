fd7e7d18b51c99f7f4ea600c79de39d0cead1035
Divyanshi Agrawal <divsagr3@gmail.com>
2020-08-31 00:06:15 +0530

    posting

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { db, auth } from "./firebase";
 import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 import { Button, Input } from "@material-ui/core";
+import ImageUpload from "./ImageUpload";
 
 function getModalStyle() {
   const top = 50;
@@ -99,6 +100,8 @@ function App() {
 
   return (
     <div className="App">
+      <ImageUpload />
+
       <Modal open={open} onClose={() => setOpen(false)}>
         <div style={modalStyle} className={classes.paper}>
           <form className="app-signup">
--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function ImageUpload() {
-  return <div></div>;
+  return (
+    <div>
+      <input type="text" />
+      <input type="file" onChange={handleChnage} />
+      <Button onClick={handleUpload}>Upload</Button>
+    </div>
+  );
 }
 
 export default ImageUpload;
