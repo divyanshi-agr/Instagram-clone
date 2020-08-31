87970098d306b3ce673ea8267f4f2cf10afb6555
Divyanshi Agrawal <divsagr3@gmail.com>
2020-08-31 22:10:29 +0530

    storing images

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
 
 function ImageUpload() {
+  const [image, setImage] = useState("");
   const [caption, setCaption] = useState("");
+  const [progress, setProgress] = useState(0);
+
+  const handleChange = (e) => {
+    if (e.target.files[0]) {
+      setImage(e.target.files[0]);
+    }
+  }
 
   return (
     <div>
-      <input type="text" placeholder="Enter a caption..." />
+      <input type="text" placeholder="Enter a caption..." onChange={event => setCaption(event.target.value)} value={} />
       <input type="file" onChange={handleChange} />
       <Button onClick={handleUpload}>Upload</Button>
     </div>
